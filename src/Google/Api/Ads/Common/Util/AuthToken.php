589284b477d81ca2a095a6f701c517aaafd455bd
api.ekoleda@gmail.com <api.ekoleda@gmail.com@bd846e36-b2c2-11de-ae6a-cd50274c347e>
2010-05-26 19:59:26 +0000

    Improved error handling for AuthToken requests.
    git-svn-id: http://google-api-adwords-php.googlecode.com/svn/trunk@67 bd846e36-b2c2-11de-ae6a-cd50274c347e

--- src/Google/Api/Ads/Common/Util/AuthToken.php
+++ src/Google/Api/Ads/Common/Util/AuthToken.php
@@ -27,8 +27,9 @@
  * @license    http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
  * @author     Adam Rogal <api.arogal@gmail.com>
  * @author     Eric Koleda <api.ekoleda@gmail.com>
- * @author     Eric Koleda <api.ekoleda@gmail.com>
  */
+
+require_once 'AuthTokenException.php';
 
 /**
  * Client used to retrieve an authentication token for the supplied credentials
@@ -36,70 +37,79 @@
  */
 class AuthToken {
   private $email;
-  private $passwd;
+  private $password;
   private $accountType;
   private $service;
   private $source;
   private $server;
-  private $res;
+  private $captchaToken;
+  private $captchaResponse;
 
   /**
    * Constructor for the authentication token.
    * @param string $email the email of the user
-   * @param string $passwd the password of the user
+   * @param string $password the password of the user
    * @param string $service the service name
    * @param string $source the source name
    * @param string $accountType the account type. Defaults to GOOGLE
    * @param string $server the server to make the request to. Defaults to
    *     https://www.google.com
+   * @param string captchaToken the token return with a CAPTCHA challenge
+   * @param string captchaResponse the response to a CAPTCHA challenge
    */
-  function __construct($email, $passwd, $service,
-      $source, $accountType = 'GOOGLE', $server = NULL) {
+  function __construct($email, $password, $service, $source,
+      $accountType = 'GOOGLE', $server = NULL, $captchaToken = NULL,
+      $captchaResponse = NULL) {
     $this->email = $email;
-    $this->passwd = $passwd;
+    $this->password = $password;
     $this->accountType = $accountType;
     $this->service = $service;
     $this->source = $source;
     $this->server = isset($server) ? $server : 'https://www.google.com';
-  }
-
-  /**
-   * Gets the token from the client login API result.
-   * @return string the auth token
-   * @access private
-   */
-  private function GetToken() {
-    $lines = explode("\n", $this->res);
-    foreach ($lines as $line) {
-      $parts = explode('=', $line);
-      if ($parts[0] == 'Auth') {
-        return $parts[1];
-      }
-    }
-
-    throw new InvalidArgumentException(
-        'The login request used a username or password that is not'
-        . ' recognized.');
+    $this->captchaToken = $captchaToken;
+    $this->captchaResponse = $captchaResponse;
   }
 
   /**
    * Peforms a POST to get the auth token and then parses the result.
    * @return string the auth token
+   * @throws AuthTokenException if an error occurs during authentication
    */
   public function GetAuthToken() {
-    $this->Login();
-    return $this->GetToken();
+    $response = $this->Login();
+    $fields = $this->ParseResponse($response);
+    if (array_key_exists('Error', $fields)) {
+      $error = $fields['Error'];
+      $url = array_key_exists('Url', $fields) ? $fields['Url'] : NULL;
+      $captchaToken = array_key_exists('CaptchaToken', $fields) ?
+          $fields['CaptchaToken'] : NULL;
+      $captchaUrl = array_key_exists('CaptchaUrl', $fields) ?
+          $fields['CaptchaUrl'] : NULL;
+      throw new AuthTokenException($error, $url, $captchaToken, $captchaUrl);
+    } else if (!array_key_exists('Auth', $fields)) {
+      throw new AuthTokenException('Unknown');
+    } else {
+      return $fields['Auth'];
+    }
   }
 
   /**
    * Makes the client login requrest and stores the result.
+   * @return string the response from the ClientLogin API
+   * @throws AuthTokenException if an error occurs during authentication
    * @access private
    */
   private function Login() {
     $postUrl = $this->server . '/accounts/ClientLogin';
-    $postVars = 'accountType=' . $this->accountType . '&Email='
-        . urlencode($this->email) . '&Passwd=' . urlencode($this->passwd)
-        . '&service=' . $this->service . '&source=' . $this->source;
+    $postVars = http_build_query(array(
+        'accountType' => $this->accountType,
+        'Email' => $this->email,
+        'Passwd' => $this->password,
+        'service' => $this->service,
+        'source' => $this->source,
+        'logintoken' => $this->captchaToken,
+        'logincaptcha' => $this->captchaResponse
+    ));
 
     $ch = curl_init($postUrl);
     curl_setopt($ch, CURLOPT_POST, 1);
@@ -119,7 +129,41 @@ class AuthToken {
       curl_setopt($ch, CURLOPT_PROXYUSERPWD, HTTP_PROXY_USER . ':'
           . HTTP_PROXY_PASSWORD);
     }
-    $this->res = curl_exec($ch);
+    $response = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $error = curl_error($ch);
     curl_close($ch);
+    if (!empty($error)) {
+      throw new AuthTokenException($error);
+    } else if ($httpCode != 200 && $httpCode != 403) {
+      throw new AuthTokenException($httpCode);
+    }
+    return $response;
+  }
+
+  /**
+   * Parses the response into a map of field name to value.
+   * @param string $response the response from the ClientLogin API
+   * @return array a map of field name to value
+   * @access private
+   */
+  private function ParseResponse($response) {
+    $result = array();
+    $lines = explode("\n", $response);
+    foreach ($lines as $line) {
+      $parts = explode('=', $line, 2);
+      $key = isset($parts[0]) ? $parts[0] : NULL;
+      $value = isset($parts[1]) ? $parts[1] : NULL;
+      $result[$key] = $value;
+    }
+    return $result;
+  }
+
+  /**
+   * Returns the server to make requests to.
+   * @return string the server to make requests to
+   */
+  public function GetServer() {
+    return $this->server;
   }
 }
