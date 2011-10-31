e2e90a24f2bb4b66199e9fc02b2688a92cf3011b
eric.koleda@google.com <eric.koleda@google.com@bd846e36-b2c2-11de-ae6a-cd50274c347e>
2011-10-31 17:57:37 +0000

    Added AdX examples for v201109.
    git-svn-id: http://google-api-adwords-php.googlecode.com/svn/trunk@218 bd846e36-b2c2-11de-ae6a-cd50274c347e

---
+++ adx-examples/v201109/CheckCampaigns.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * This example shows how to use the validate only header to check for errors.
+ * No objects will be created, but exceptions will still be thrown.
+ *
+ * Tags: CampaignService.mutate
+ *
+ * PHP version 5
+ *
+ * Copyright 2011, Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package    GoogleApiAdsAdWords
+ * @subpackage v201109
+ * @category   WebServices
+ * @copyright  2011, Google Inc. All Rights Reserved.
+ * @license    http://www.apache.org/licenses/LICENSE-2.0 Apache License,
+ *             Version 2.0
+ * @author     Eric Koleda <api.ekoleda@gmail.com>
+ */
+
+error_reporting(E_STRICT | E_ALL);
+
+// You can set the include path to src directory or reference
+// AdWordsUser.php directly via require_once.
+// $path = '/path/to/aw_api_php_lib/src';
+$path = dirname(__FILE__) . '/../../src';
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+
+require_once 'Google/Api/Ads/AdWords/Lib/AdWordsUser.php';
+
+try {
+  // Get AdWordsUser from credentials in "../auth.ini"
+  // relative to the AdWordsUser.php file's directory.
+  $user = new AdWordsUser();
+
+  // Log SOAP XML request and response.
+  $user->LogDefaults();
+
+  // Get the validation CampaignService by passing true for the parameter
+  // $validateOnly.
+  $campaignValidationService =
+      $user->GetService('CampaignService', 'v201109', NULL, NULL, TRUE);
+
+  // Create campaign.
+  $goodCampaign = new Campaign();
+  $goodCampaign->name = 'Campaign #' + time();
+  $goodCampaign->status = 'PAUSED';
+  $goodCampaign->biddingStrategy = new ManualCPM();
+
+  // Create budget.
+  $budget = new Budget();
+  $budget->period = 'DAILY';
+  $budget->amount = new Money(50000000);
+  $budget->deliveryMethod = 'STANDARD';
+  $goodCampaign->budget = $budget;
+
+  // Create operations.
+  $operation = new CampaignOperation();
+  $operation->operand = $goodCampaign;
+  $operation->operator = 'ADD';
+
+  $operations = array($operation);
+
+  // Validate campaign operation.
+  $result = $campaignValidationService->mutate($operations);
+
+  // Display new campaigns, which should be none if the service was a
+  // validation service.
+  if (isset($result->value)) {
+    foreach ($result->value as $campaign) {
+      print 'New campaign with name "' . $campaign->name
+          . '" and id "' . $campaign->id . "\" was created.\n";
+    }
+  } else {
+    print "No campaigns were created.\n";
+  }
+
+  // Provide an invalid bidding strategy that will cause an exception
+  // durning validation.
+  $badCampaign = new Campaign();
+  $badCampaign->name = 'Campaign #' + time();
+  $badCampaign->status = 'PAUSED';
+  $badCampaign->budget = $budget;
+
+  // Throws RequiredError.REQUIRED @ operations[0].operand.biddingStrategy.
+  $badCampaign->biddingStrategy = NULL;
+
+  // Create operations.
+  $operation = new CampaignOperation();
+  $operation->operand = $badCampaign;
+  $operation->operator = 'ADD';
+
+  $operations = array($operation);
+
+  try {
+    // Validate campaign operation.
+    $result = $campaignValidationService->mutate($operations);
+  } catch (Exception $e) {
+    print 'Validation failed for reason "' . $e->getMessage() . "\".\n";
+  }
+} catch (Exception $e) {
+  print $e->getMessage();
+}
